d9ef11ab38b4df59243a520c34ae2b3e27b28d27
Lupanar <69lupanar@proton.me>
2025-07-12 20:19:07 +0200

    added dotween package + camera controller

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,17 +1,24 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Transform[] _waypoints;
+
+    Transform _curWaypoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        GameManager.OnProgressChangedEvent += OnProgressChangedCallback;
+        OnProgressChangedCallback(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnProgressChangedCallback(int progress)
     {
-
+        _curWaypoint = _waypoints[GameManager.Progress % _waypoints.Length];
+        _camera.transform.DOMove(_curWaypoint.position, 1f);
+        _camera.transform.DORotate(_curWaypoint.rotation.eulerAngles, 1f);
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,16 +1,43 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static Action<int> OnProgressChangedEvent;
+
+    public static int Progress { get; private set; }
+
+    [SerializeField] private int ProgressToWin = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Progress = 0;
+        OnProgressChangedEvent += OnProgressChangedCallback;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && Progress < ProgressToWin)
+        {
+            RiseProgress();
+        }
+    }
+
+    private void RiseProgress()
+    {
+        Progress++;
+        OnProgressChangedEvent?.Invoke(Progress);
+    }
+
+    private void OnProgressChangedCallback(int progress)
+    {
+        print(progress);
+
+        if (progress == ProgressToWin)
+        {
+            print("You win!");
+        }
     }
 }
